39966f3cc9d8950a2ee1b69f52f4147bd6fe571b
vavan19911987 <vavan7520@gmail.com>
2022-11-30 12:44:23 +0300

    add gitkraken

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,17 @@
 "use strict";
 
+let num1 = 3;
+console.log(num1);
+num1++;
+console.log(num1);
 
+let num2 = num1--;
+console.log(num1);
+console.log(num2);
+
+
+console.log(num1++);
+console.log(--num2);
+
+let num = 3;
+alert(--num);
